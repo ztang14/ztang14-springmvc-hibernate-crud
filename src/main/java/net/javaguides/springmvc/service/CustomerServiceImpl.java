8e6558e290afcec19b365489bf1946e89a9fa37b
ztang14 <87488856+ztang14@users.noreply.github.com>
2022-02-09 06:57:10 +0800

    Add files via upload

--- src/main/java/net/javaguides/springmvc/service/CustomerServiceImpl.java
+++ src/main/java/net/javaguides/springmvc/service/CustomerServiceImpl.java
@@ -48,14 +48,7 @@ public class CustomerServiceImpl implements CustomerService {
         return customerDAO.getTotalNumber();
     }
 
-    /**
-     * 分页查询数据
-     *
-     * @param startId 起始数据ID参数
-     * @param page    查询页码，从1开始
-     * @param size    每页查询数量
-     * @return 查询的数据列表
-     */
+
     @Override
     public List<Customer> pageQueryCustomers(int startId, int page, int size) {
         return customerDAO.pageCustomers(startId, page, size);
